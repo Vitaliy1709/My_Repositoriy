abadef93706abed188994eeadcebe286b44c6922
Vitaliy1709 <63902563+Vitaliy1709@users.noreply.github.com>
2023-12-22 12:23:48 +0000

    Update new_file.py

--- New/new_file.py
+++ New/new_file.py
@@ -6,6 +6,6 @@ data = res.json()
 
 new_data = ["".join(i['title'].capitalize() + "\n" + i['body'].capitalize() + "." + "\n\n" for i in data)]
 
-with open("C:/Users/user 79/My_Repositoriy/New/new_text.txt", "w") as file:
+with open("C:/Users/new_text.txt", "w") as file:
     for j in new_data:
         file.write(str(j))
